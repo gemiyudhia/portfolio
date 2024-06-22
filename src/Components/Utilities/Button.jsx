cc943b05f090f0cdb546e527c65de039a6cbf750
gemiyudhia <gemiyudhiaa@gmail.com>
2024-06-22 19:37:14 +0700

    menambahkan form contact dan juga menginstall radix ui

--- src/Components/Utilities/Button.jsx
+++ src/Components/Utilities/Button.jsx
@@ -1,11 +1,11 @@
 import React, { Children } from "react";
 import { Link } from "react-router-dom";
 
-export const Button = ({ children = "...", to }) => {
+export const Button = ({ children = "...", to, variant }) => {
   return (
     <Link
       to={to}
-      className="text-sm bg-gradient-to-r from-cyan-500 to-blue-500 p-3 rounded-lg font-semibold">
+      className={`${variant} text-sm bg-gradient-to-r from-cyan-500 to-blue-500 p-3 rounded-lg font-semibold`}>
       {children}
     </Link>
   );
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Header } from "../Components/Header";
-import { Contact } from "../Components/Contact";
+import { ContactForm } from "../Components/Contact";
 
 export const ContactPage = () => {
   return (
     <>
       <Header />
-      <Contact />
+      <ContactForm />
     </>
   );
 };
