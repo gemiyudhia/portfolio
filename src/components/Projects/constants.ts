423c5bf74bd62b48f8218e09ca6b996e566a6cbe
Gemi <githuba122@gmail.com>
2025-02-07 19:04:40 +0700

    feat: add project

---
+++ src/components/Projects/constants.ts
@@ -0,0 +1,63 @@
+export const PROJECTS_DATA = [
+  // movie website
+  {
+    title: "Movies App",
+    description:
+      "This is a Movie Search Application built using React, Vite, TypeScript, Tailwind CSS, Headless UI, and GSAP for animations. The app enables users to search for movies and explore various categories, such as popular movies, top-rated movies, and more.",
+    images: [
+      "/images/movies/1.jpeg",
+      "/images/movies/2.jpeg",
+      "/images/movies/3.png",
+    ],
+    demoLink: "https://movie-project-sandy-nu.vercel.app/",
+    repoLink: "https://github.com/gemiyudhia/new-movie-project",
+  },
+
+  // online store
+  {
+    title: "Online Store",
+    description:
+      "This is an e-commerce web application built with modern technologies to provide a seamless shopping experience. The application supports product browsing, user authentication.",
+    images: [
+      "/images/onlinestore/1.jpeg",
+      "/images/onlinestore/2.jpeg",
+      "/images/onlinestore/3.jpeg",
+      "/images/onlinestore/4.jpeg",
+      "/images/onlinestore/5.jpeg",
+    ],
+    demoLink: "https://toko-online-mu.vercel.app/",
+    repoLink: "https://github.com/gemiyudhia/onlinestore",
+  },
+
+  // socialfy
+  {
+    title: "Socialfy",
+    description:
+      "Socialfy is a social media web application. User can create account, post, like, comment.",
+    images: [
+      "/images/socialfy/1.jpeg",
+      "/images/socialfy/2.jpeg",
+      "/images/socialfy/3.jpeg",
+      "/images/socialfy/4.jpeg",
+      "/images/socialfy/5.jpeg",
+    ],
+    demoLink: "https://socialfy-kohl.vercel.app/",
+    repoLink: "https://github.com/gemiyudhia/socialfy",
+  },
+
+  // talky
+  {
+    title: "Talky",
+    description:
+      "Talky is a Next.js-based application that supports user authentication, real-time communication, and Firebase integration. This project leverages modern technologies to provide a responsive and interactive user experience.",
+    images: [
+      "images/talky/1.png",
+      "images/talky/2.png",
+      "images/talky/3.png",
+      "images/talky/4.png",
+      "images/talky/5.png",
+    ],
+    demoLink: "https://talky-three.vercel.app/",
+    repoLink: "https://github.com/gemiyudhia/talky",
+  },
+] as const;
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,160 +1,82 @@
 import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/ScrollTrigger"; // Tambahkan ini
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import gsap from "gsap";
 import { useRef } from "react";
+import { ProjectCarousel } from "./ProjectCarousel";
+import { PROJECTS_DATA } from "./constants";
 
-gsap.registerPlugin(ScrollTrigger); // Daftarkan plugin ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
-  const projectRef = useRef<HTMLDivElement>(null);
-  const projectOneRef = useRef<HTMLImageElement>(null);
-  const projectTwoRef = useRef<HTMLImageElement>(null);
-  const projectThreeRef = useRef<HTMLImageElement>(null);
-  const projectFourRef = useRef<HTMLImageElement>(null);
-  const projectFiveRef = useRef<HTMLImageElement>(null);
+  const refs = {
+    project: useRef<HTMLDivElement>(null),
+    cards: Array(4)
+      .fill(null)
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      .map(() => useRef<HTMLDivElement>(null)),
+  };
 
   useGSAP(() => {
+    const scrollConfig = {
+      start: "top 80%",
+      end: "bottom 20%",
+      toggleActions: "play none none reverse",
+      scroller: ".scrollbar",
+    };
+
     gsap.fromTo(
-      projectRef.current,
-      { opacity: 0, y: 50 }, // Mulai tersembunyi
+      refs.project.current,
+      { opacity: 0, y: 50 },
       {
         opacity: 1,
         y: 0,
         duration: 1,
         ease: "power3.out",
-        scrollTrigger: {
-          trigger: projectRef.current,
-          start: "top 80%",
-          end: "bottom 20%",
-          toggleActions: "play none none reverse",
-          scroller: ".scrollbar",
-        },
+        scrollTrigger: scrollConfig,
       }
     );
-    gsap.fromTo(
-      projectOneRef.current,
-      { opacity: 0, x: -50 }, // Mulai tersembunyi
-      {
-        opacity: 1,
-        x: 0,
-        duration: 1,
-        ease: "power3.out",
-        scrollTrigger: {
-          trigger: projectOneRef.current,
-          start: "top 80%",
-          end: "bottom 20%",
-          toggleActions: "play none none reverse",
-          scroller: ".scrollbar",
-        },
-      }
-    );
-    gsap.fromTo(
-      projectTwoRef.current,
-      { opacity: 0, x: 50 }, // Mulai tersembunyi
-      {
-        opacity: 1,
-        x: 0,
-        duration: 1,
-        ease: "power3.out",
-        scrollTrigger: {
-          trigger: projectTwoRef.current,
-          start: "top 80%",
-          end: "bottom 20%",
-          toggleActions: "play none none reverse",
-          scroller: ".scrollbar",
-        },
-      }
-    );
-    gsap.fromTo(
-      projectThreeRef.current,
-      { opacity: 0, y: 50 }, // Mulai tersembunyi
-      {
-        opacity: 1,
-        y: 0,
-        duration: 2,
-        ease: "power3.out",
-        scrollTrigger: {
-          trigger: projectThreeRef.current,
-          start: "top 80%",
-          end: "bottom 20%",
-          toggleActions: "play none none reverse",
-          scroller: ".scrollbar",
-        },
-      }
-    );
-    gsap.fromTo(
-      projectFourRef.current,
-      { opacity: 0, y: 50 }, // Mulai tersembunyi
-      {
-        opacity: 1,
-        y: 0,
-        duration: 3,
-        ease: "power3.out",
-        scrollTrigger: {
-          trigger: projectFourRef.current,
-          start: "top 80%",
-          end: "bottom 20%",
-          toggleActions: "play none none reverse",
-          scroller: ".scrollbar",
-        },
-      }
-    );
-    gsap.fromTo(
-      projectFiveRef.current,
-      { opacity: 0, y: 50 }, // Mulai tersembunyi
-      {
-        opacity: 1,
-        y: 0,
-        duration: 3.5,
-        ease: "power3.out",
-        scrollTrigger: {
-          trigger: projectFiveRef.current,
-          start: "top 80%",
-          end: "bottom 20%",
-          toggleActions: "play none none reverse",
-          scroller: ".scrollbar",
-        },
-      }
-    );
+
+    refs.cards.forEach((ref, index) => {
+      gsap.fromTo(
+        ref.current,
+        { opacity: 0, y: 50 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1 + index * 0.5,
+          ease: "power3.out",
+          scrollTrigger: { ...scrollConfig, trigger: ref.current },
+        }
+      );
+    });
   }, []);
 
   return (
     <section className="md:pt-28 pt-[100px]">
       <h1
-        ref={projectRef}
+        ref={refs.project}
         className="text-3xl text-secondary uppercase text-center font-black mb-9"
       >
-        my project
+        my projects
       </h1>
       <div className="grid grid-cols-4 lg:grid-cols-5 grid-rows-4 gap-7">
-        {/* Gambar pertama */}
-        <img
-          ref={projectOneRef}
-          src=""
-          alt="1"
-          className="bg-primary rounded-3xl w-full h-96 col-span-4 row-span-2 lg:col-span-3 shadow-thick-solid"
-        />
-        {/* Gambar kedua */}
-        <img
-          ref={projectTwoRef}
-          src=""
-          alt="2"
-          className="bg-primary rounded-3xl w-full h-96 col-span-4 md:col-span-2 row-span-2 shadow-thick-solid"
-        />
-        {/* Gambar ketiga */}
-        <img
-          ref={projectFourRef}
-          src=""
-          alt="4"
-          className="bg-primary rounded-3xl w-full h-96 col-span-4 row-span-2 md:col-span-2 lg:row-span-2 shadow-thick-solid"
-        />
-        {/* Gambar kelima */}
-        <img
-          ref={projectFiveRef}
-          src=""
-          alt="5"
-          className="bg-primary rounded-3xl w-full h-96 col-span-4 row-span-2 lg:col-span-3 shadow-thick-solid"
-        />
+        {PROJECTS_DATA.map((project, index) => (
+          <div
+            key={project.title}
+            ref={refs.cards[index]}
+            className={`bg-primary rounded-3xl w-full h-96 col-span-4 row-span-2 ${
+              index === 0
+                ? "lg:col-span-3"
+                : index === 1
+                ? "md:col-span-2"
+                : index === 2
+                ? "md:col-span-2 lg:row-span-2"
+                : "lg:col-span-3"
+            } shadow-thick-solid overflow-hidden`}
+          >
+            <ProjectCarousel {...project} />
+          </div>
+        ))}
       </div>
     </section>
   );
